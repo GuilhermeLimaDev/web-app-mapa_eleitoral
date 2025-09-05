4584a6008754cb90917ea144d9668fc441f543e9
guilherme lima <guilhermeguiguigugu2509@gmail.com>
2025-09-05 18:18:55 +0000

    adiciona componente AppRoutes com configuração inicial de rotas

---
+++ mapa-eleitoral_v1.0/src/components/AppRoutes.jsx
@@ -0,0 +1,13 @@
+import {Routes, Route, Router} from 'react-router-dom'
+
+const AppRoutes = () =>{
+    return(
+        <Router>
+            <Routes>
+                
+            </Routes>
+        </Router>
+    )
+}
+
+export default AppRoutes
