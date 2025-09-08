4ed20ca02da7033fc9fa2f29577ab2bd7fd674ee
guilherme lima <guilhermeguiguigugu2509@gmail.com>
2025-09-08 18:34:49 +0000

    adiciona rota inicial para o componente de Login

--- mapa-eleitoral_v1.0/src/components/AppRoutes.jsx
+++ mapa-eleitoral_v1.0/src/components/AppRoutes.jsx
@@ -1,10 +1,11 @@
 import {Routes, Route, Router} from 'react-router-dom'
+import Login from '../pages/login/Login'
 
 const AppRoutes = () =>{
     return(
         <Router>
             <Routes>
-                
+                <Route path='/' element={<Login/>}/>
             </Routes>
         </Router>
     )
